ffa1b0312bd9e7afeb293d4d0aae9849e109256d
Velin Apostolov <velin.apostolove@gmail.com>
2025-05-15 19:44:37 +0300

    fetches correct backend provider depending on the browser link

--- client/src/data/api.js
+++ client/src/data/api.js
@@ -1,4 +1,9 @@
-export const PROXY_BASE_URL = "http://localhost:3001/api";
+const isLocalhost = window.location.hostname === "localhost";
+
+export const PROXY_BASE_URL = isLocalhost
+    ? "http://localhost:3001/api"
+    : import.meta.env.VITE_API_URL;
+
 
 export async function getAppointments() {
     const res = await fetch(`${PROXY_BASE_URL}/appointments`);
